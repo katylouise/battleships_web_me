eddb80baa0c7957465f87e3d7f5089da7181b2bd
Rebecca Appleyard <rklappleyard@gmail.com>
2015-08-06 10:57:47 +0100

    RSpec test for firing

--- spec/features/playing_a_game_spec.rb
+++ spec/features/playing_a_game_spec.rb
@@ -5,6 +5,7 @@ feature 'Setting up a game' do
     visit '/game'
     expect(page).to have_content('ABCDEFGHIJ')
   end
+
   scenario 'Placing a ship' do
     visit '/game'
     fill_in('coordinate', with: 'A2')
@@ -13,4 +14,19 @@ feature 'Setting up a game' do
     click_button('Submit')
     expect($game.own_board_view($game.player_1)).to have_content('DD')
   end
+
+   scenario 'Firing at your opponent' do
+    visit '/gameplay'
+    fill_in('coordinate', with: 'A2')
+    click_button('Fire!')
+    expect(page).to have_content('sunk')
+  end
+
+  # scenario 'Firing at your opponent' do
+  #   visit '/gameplay'
+  #   fill_in('coordinate', with: 'A1')
+  #   click_button('Fire!')
+  #   expect(page).to have_content('miss')
+  # end
+
 end
